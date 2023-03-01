defc1c58ddc821892c38a2e9e67ac984a5f322f4
ryanlleung <57994878+ryanlleung@users.noreply.github.com>
2023-03-01 22:26:48 +0000

    Basic upscaled

--- gui.py
+++ gui.py
@@ -7,6 +7,11 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtMultimedia import *
 from PyQt5.QtMultimediaWidgets import *
 
+import cv2
+from cv2 import dnn_superres
+from img_ops import upscale_img
+from PIL import Image
+
 
 class MainWindow(QWidget):
 
@@ -18,46 +23,53 @@ class MainWindow(QWidget):
         
         ### Before ###
 
-        self.before_layout = QVBoxLayout()
-        self.before_layout.setContentsMargins(0, 0, 0, 0)
         self.before_title = QLabel('Original')
         self.before_title.setFont(QFont('Helvetica', 10))
+
         self.before_img = QLabel()
         self.before_img.setAlignment(Qt.AlignCenter)
+        self.before_img.setFixedSize(400, 400)
         self.before_img.setStyleSheet("background-color: black;")
+
+        self.before_layout = QVBoxLayout()
+        self.before_layout.setContentsMargins(0, 0, 0, 0)
         self.before_layout.addWidget(self.before_title)
         self.before_layout.addWidget(self.before_img)
+
         self.before_widget = QWidget()
         self.before_widget.setLayout(self.before_layout)
 
-        pixmap = QPixmap('./medias/Array.png')
+        pixmap = QPixmap()
         pixmap = pixmap.scaled(self.before_img.size(), aspectRatioMode=Qt.KeepAspectRatio, transformMode=Qt.SmoothTransformation)
         self.before_img.setPixmap(pixmap)
 
         ### After ###
 
-        self.after_layout = QVBoxLayout()
-        self.after_layout.setContentsMargins(0, 0, 0, 0)
         self.after_title = QLabel('Result')
         self.after_title.setFont(QFont('Helvetica', 10))
+
         self.after_img = QLabel()
         self.after_img.setAlignment(Qt.AlignCenter)
+        self.after_img.setFixedSize(400, 400)
         self.after_img.setStyleSheet("background-color: black;")
+
+        self.after_layout = QVBoxLayout()
+        self.after_layout.setContentsMargins(0, 0, 0, 0)
         self.after_layout.addWidget(self.after_title)
         self.after_layout.addWidget(self.after_img)
+
         self.after_widget = QWidget()
         self.after_widget.setLayout(self.after_layout)
 
-        pixmap = QPixmap('./medias/Non-motorised.png')
+        pixmap = QPixmap()
         pixmap = pixmap.scaled(self.after_img.size(), aspectRatioMode=Qt.KeepAspectRatio, transformMode=Qt.SmoothTransformation)
         self.after_img.setPixmap(pixmap)
 
         ### Arrow ###
 
-        self.arrow = QLabel('→')
-        self.arrow.setAlignment(Qt.AlignCenter)
-        self.arrow.setFont(QFont('Arial', 15))
-        self.arrow.setStyleSheet("font-weight: bold")
+        self.arrow = QPushButton('→')
+        self.arrow.setFixedWidth(50)
+        self.arrow.clicked.connect(self.onArrowClicked)
 
         ##### Media #####
 
@@ -77,8 +89,7 @@ class MainWindow(QWidget):
         self.queue_title.setFont(QFont('Helvetica', 10))
 
         self.queue_list = QListWidget()
-        self.queue_list.addItem('Item 1')
-        self.queue_list.addItems(['Item 2', 'Item 3', 'Item 4'])
+        self.queue_list.itemClicked.connect(self.onQueueItemClicked)
 
         self.queue_layout = QVBoxLayout()
         self.queue_layout.setContentsMargins(0, 0, 0, 0)
@@ -89,12 +100,39 @@ class MainWindow(QWidget):
         self.queue.setLayout(self.queue_layout)
 
         ### Console ###
+        
+        self.console_title = QLabel('Console')
+        self.console_title.setFont(QFont('Helvetica', 10))
 
-        # Create 
+        # Create display box
+        self.console_display = QTextEdit()
+        self.console_display.setReadOnly(True)
+        self.console_display.setLineWrapMode(QTextEdit.NoWrap)
+        self.console_display.setFixedHeight(100)
+
+        # Create file selection button
+        self.file_select = QPushButton('Select File')
+        self.file_select.clicked.connect(self.onFileSelectClicked)
+
+        # Create clear queue button
+        self.clear_queue = QPushButton('Clear Queue')
+        self.clear_queue.clicked.connect(self.onClearQueueClicked)
+
+        # Create buttons layout
+        self.file_buttons_layout = QHBoxLayout()
+        self.file_buttons_layout.setContentsMargins(0, 0, 0, 0)
+        self.file_buttons_layout.addWidget(self.file_select)
+        self.file_buttons_layout.addWidget(self.clear_queue)
+
+        self.file_buttons = QWidget()
+        self.file_buttons.setLayout(self.file_buttons_layout)
 
         # Create console widget
         self.console_layout = QVBoxLayout()
         self.console_layout.setContentsMargins(0, 0, 0, 0)
+        self.console_layout.addWidget(self.console_title)
+        self.console_layout.addWidget(self.console_display)
+        self.console_layout.addWidget(self.file_buttons)
 
         self.console = QWidget()
         self.console.setLayout(self.console_layout)
@@ -194,16 +232,61 @@ class MainWindow(QWidget):
         ###---------Window---------###
 
         QApplication.setStyle(QStyleFactory.create('Fusion'))
-        self.setGeometry(1600, 100, 750, 500)
+        self.setGeometry(2000, 100, 750, 500)
         self.setWindowTitle('AI Upscaler')
         self.show()
 
+    # Function to handle when the arrow is clicked, upscale image/video
+    def onArrowClicked(self):
+
+        print("arrow clicked")
+        img = cv2.imread(self.queue_list.item(0).text())
+
+        # Find which radio is checked
+        if self.scale_radio_1.isChecked():
+            scale = 2
+        elif self.scale_radio_2.isChecked():
+            scale = 3
+        elif self.scale_radio_3.isChecked():
+            scale = 4
+        elif self.scale_radio_4.isChecked():
+            scale = 8
+
+        # Models
+        if self.model_combo.currentIndex() == 0:
+            model = "edsr"
+        elif self.model_combo.currentIndex() == 1:
+            model = "espcn"
+        elif self.model_combo.currentIndex() == 2:
+            model = "fsrcnn"
+        elif self.model_combo.currentIndex() == 3:
+            model = "lapsrn"
+
+        upscaled = upscale_img(img, model, scale)
+
+        # Display upscaled image
+        self.after_img.setPixmap(QPixmap.fromImage(QImage(upscaled, upscaled.shape[1], upscaled.shape[0], upscaled.strides[0], QImage.Format_RGB888)))
+
+    # Function to handle when the height input is changed
     def onHeightReturnPressed(self):
         print(self.input_height.text())
 
+    # Function to handle when the width input is changed
     def onWidthReturnPressed(self):
         print(self.input_width.text())
 
+    # Function to open file explorer to select an image/video
+    def onFileSelectClicked(self):
+        self.queue_list.addItem(QFileDialog.getOpenFileName(self, 'Select File', os.getenv('HOME'))[0])
+
+    # Function to handle clearing the queue
+    def onClearQueueClicked(self):
+        self.queue_list.clear()
+
+    # Function to handle when an item in the queue is clicked
+    def onQueueItemClicked(self, item):
+        print("queue clicked")
+        self.before_img.setPixmap(QPixmap(item.text()))
 
             
 def main():
@@ -213,22 +296,3 @@ def main():
 	
 if __name__ == '__main__':
     main()
-
-
-
-"""# Set control layout
-controlLayout = QHBoxLayout()
-controlLayout.setContentsMargins(0, 0, 0, 0)
-controlLayout.addWidget(self.play_button)
-controlLayout.addWidget(self.time_label)
-controlLayout.addWidget(self.time_slider)
-controlLayout.addWidget(self.volume_slider)
-
-# Set layout for video player
-vp_layout = QVBoxLayout()
-vp_layout.addWidget(self.video_widget)
-vp_layout.addLayout(controlLayout)
-
-# Set widget for video player
-self.vp_widget = QWidget()
-self.vp_widget.setLayout(vp_layout)"""
